82d0f8b9f3a64c916594d997d0e006ea82d8dcc3
JohnK0 <johnskim97@gmail.com>
2019-03-21 14:35:32 -0700

    changed fragement background color

--- app/src/main/java/com/hci/nudgeswype/reminderFragment.kt
+++ app/src/main/java/com/hci/nudgeswype/reminderFragment.kt
@@ -16,7 +16,7 @@ class reminderFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.reminder,container,false)
         daysOfTheWeek = booleanArrayOf(false,false,false,false,false,false,false)
-        val onSwitch =  view.findViewById<Switch>(R.id.toggleSwitch)
+/*        val onSwitch =  view.findViewById<Switch>(R.id.toggleSwitch)
         onSwitch.setOnCheckedChangeListener({ _, isChecked ->
             if (isChecked) {
                 view.setBackgroundColor(Color.rgb(51, 181, 229))
@@ -24,7 +24,7 @@ class reminderFragment : Fragment() {
 
                 view.setBackgroundColor(Color.LTGRAY)
             }
-        })
+        })*/
         return view
     }
 
