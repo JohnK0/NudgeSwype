1bfa591b88bd861e2f3e35c6088ec68fa6dd851f
dsamuel27 <david.samuel1227@gmail.com>
2019-03-22 11:21:05 -0700

    Started logic for dynamically adding fragments.

--- app/src/main/java/com/hci/nudgeswype/MainActivity.kt
+++ app/src/main/java/com/hci/nudgeswype/MainActivity.kt
@@ -1,21 +1,39 @@
 package com.hci.nudgeswype
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    var str = "0"
+    val fragManager = supportFragmentManager
+    val fragTransaction = fragManager.beginTransaction()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //if (savedInstanceState!!.getBoolean("AddFragment")) {
+           // add(reminderFragment())
+        //}
         setContentView(R.layout.activity_main)
+        //val fragManager = supportFragmentManager
+        //val fragTransaction = fragManager.beginTransaction()
 
         addReminder.setOnClickListener{
             val intent = Intent(this, AddReminder::class.java)
             startActivity(intent)
         }
     }
+
+    override fun onResume() {
+        super.onResume()
+        add(reminderFragment())
+    }
+    fun add(reminderFrag: reminderFragment) {
+        fragTransaction.add(reminderFrag, str)
+        fragTransaction.commit()
+        str += "0"
+    }
+
 
 
 }
--- app/src/main/java/com/hci/nudgeswype/reminderFragment.kt
+++ app/src/main/java/com/hci/nudgeswype/reminderFragment.kt
@@ -1,21 +1,32 @@
 package com.hci.nudgeswype
 
-import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Switch
 
 class reminderFragment : Fragment() {
 
     // array of booleans that represent days of the week
    private var daysOfTheWeek: BooleanArray? = null
+    private var daysOfTheWeekStr = arrayOf("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday", "Saturday")
+
+     fun setData(savedInstanceState: Bundle?) {
+
+        daysOfTheWeek = booleanArrayOf(false,false,false,false,false,false,false)
+
+        for (day in daysOfTheWeekStr) {
+            if (savedInstanceState!!.getBoolean(day)) {
+                setDayOfTheWeek(day, true)
+            }
+        }
+
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.reminder,container,false)
-        daysOfTheWeek = booleanArrayOf(false,false,false,false,false,false,false)
+       // daysOfTheWeek = booleanArrayOf(false,false,false,false,false,false,false)
 /*        val onSwitch =  view.findViewById<Switch>(R.id.toggleSwitch)
         onSwitch.setOnCheckedChangeListener({ _, isChecked ->
             if (isChecked) {
@@ -28,12 +39,21 @@ class reminderFragment : Fragment() {
         return view
     }
 
-    fun setDayOfTheWeekTrue(day: Int) {
-        daysOfTheWeek?.set(day, true)
+    fun setDayOfTheWeek(day: String, flag: Boolean) {
+        var index = 0
+        for (dayName in daysOfTheWeekStr) {
+            if (dayName.equals(day)) {
+
+                daysOfTheWeek?.set(index, flag);
+            }
+            index++
+        }
+
     }
 
+    /*
     fun setDayOfTheWeekFalse(day: Int) {
         daysOfTheWeek?.set(day, false)
-    }
+    } */
 
 }
